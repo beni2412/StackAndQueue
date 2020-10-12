e5dcf06d4fcea1938c4a53e096267dc8ab895bdd
beni2412 <hardaman97@gmail.com>
2020-10-12 22:58:20 +0530

    [Hardaman] Added: Pop and peek in Stack

--- src/test/java/com/capg/StackTest.java
+++ src/test/java/com/capg/StackTest.java
@@ -18,6 +18,20 @@ public class StackTest {
 		stack.push(thirdNode);
 		stack.print();
 		
+	}
+	
+	@Test
+	public void given3NumberWhenPopStackShouldBeLastAddedNode() {
+		Node<Integer> firstNode = new Node<Integer>(70);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(56);
+		MyStack stack = new MyStack();
+		stack.push(firstNode);
+		stack.push(secondNode);
+		stack.push(thirdNode);
+		INode pop = stack.pop();
+		Assert.assertEquals(thirdNode, pop);
+		stack.print();
 
 	}
 }
